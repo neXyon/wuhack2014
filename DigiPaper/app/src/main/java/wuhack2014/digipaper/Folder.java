8632ef81768bea85335a27e0c99ab978ca59044f
Jörg Müller <nexyon@gmail.com>
2014-09-13 13:43:38 -0500

    Displaying folders.

--- DigiPaper/app/src/main/java/wuhack2014/digipaper/Folder.java
+++ DigiPaper/app/src/main/java/wuhack2014/digipaper/Folder.java
@@ -1,15 +1,34 @@
 package wuhack2014.digipaper;
 
+import java.io.File;
 import java.util.List;
 
 /**
  * Created by xenon on 13.09.14.
  */
 public class Folder {
-    private List<Document> files;
-    private String path;
+    private List<Document> documents;
+    private File path;
 
-    public Folder(String path) {
+    public Folder(File path) {
         this.path = path;
+
+        File files[] = path.listFiles();
+
+        for(File file : files) {
+            if(file.isDirectory()) {
+                Document document = new Document(file);
+                documents.add(document);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return path.getName();
+    }
+
+    public File getPath() {
+        return path;
     }
 }
--- DigiPaper/app/src/main/java/wuhack2014/digipaper/FolderList.java
+++ DigiPaper/app/src/main/java/wuhack2014/digipaper/FolderList.java
@@ -6,19 +6,58 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
 public class FolderList extends Activity {
-    List<Folder> folders;
+    ArrayList<Folder> folders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        folders = new ArrayList<Folder>();
+
         setContentView(R.layout.activity_folder_list);
 
-        
+        File documents = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        documents = new File(documents, "DigiPaper");
 
+        if(!documents.exists())
+            documents.mkdir();
+
+        File subjects[] = documents.listFiles();
+
+        for(File subject : subjects) {
+            if(subject.isDirectory()) {
+                Folder folder = new Folder(subject);
+                folders.add(folder);
+            }
+        }
+
+        ListView list = (ListView)findViewById(R.id.folderView);
+
+        ArrayAdapter<Folder> adapter = new ArrayAdapter<Folder>(this, android.R.layout.simple_list_item_1, folders);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final Folder folder = (Folder) parent.getItemAtPosition(position);
+
+            }
+
+        });
     }
 
 

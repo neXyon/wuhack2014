7919b5f042e8b813396b35439b090f697331a2d3
Jörg Müller <nexyon@gmail.com>
2014-09-13 15:41:24 -0500

    Revert!

--- DigiPaper/app/src/main/java/wuhack2014/digipaper/FolderList.java
+++ DigiPaper/app/src/main/java/wuhack2014/digipaper/FolderList.java
@@ -1,24 +1,66 @@
 package wuhack2014.digipaper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
 public class FolderList extends Activity {
-    List<Folder> folders;
+    ArrayList<Folder> folders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        folders = new ArrayList<Folder>();
+
         setContentView(R.layout.activity_folder_list);
 
-        
+        File documents = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        documents = new File(documents, "DigiPaper");
 
+        if(!documents.exists())
+            documents.mkdir();
+
+        File subjects[] = documents.listFiles();
+
+        for(File subject : subjects) {
+            if(subject.isDirectory()) {
+                Folder folder = new Folder(subject);
+                folders.add(folder);
+            }
+        }
+
+        ListView list = (ListView)findViewById(R.id.folderView);
+
+        ArrayAdapter<Folder> adapter = new ArrayAdapter<Folder>(this, android.R.layout.simple_list_item_1, folders);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final Folder folder = (Folder) parent.getItemAtPosition(position);
+                Intent intent = new Intent(parent.getContext(), DocumentList.class);
+                intent.putExtra("folder", folder.getPath().getAbsolutePath());
+                startActivity(intent);
+            }
+
+        });
     }
 
 
